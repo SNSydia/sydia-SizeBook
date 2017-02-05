328d43cb9498d3c162884522eb44bc94d12ad723
SNSydia <sydia@ualberta.ca>
2017-02-05 02:17:05 -0700

    Add Person kinda working

--- app/src/main/java/ca/ualberta/cs/sizebook/Person.java
+++ app/src/main/java/ca/ualberta/cs/sizebook/Person.java
@@ -1,47 +1,8 @@
 package ca.ualberta.cs.sizebook;
 
-import java.util.Date;
-
 /**
- * Created by sydia on 1/19/17.
+ * Created by juice on 04/02/17.
  */
 
-public abstract class Person {
-    private Date date;
-
-    public String getMood() {
-        return mood;
-    }
-
-    public void setMood(String mood) {
-        this.mood = mood;
-    }
-
-    private String mood;
-    // Constructor
-    public Person(Date date, String mood) {
-        this.date = date;
-        this.mood = mood;
-    }
-
-    // Default Constructor
-    public Person(){
-        super();
-        this.date = new Date();
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String CurrentMood(){
-        return mood;
-    }
-
-    public abstract String getMoodFormatString();
-
+public class Person {
 }
--- app/src/main/java/ca/ualberta/cs/sizebook/TweetTooLongException.java
+++
@@ -1,9 +0,0 @@
-package ca.ualberta.cs.sizebook;
-
-/**
- * Created by sydia on 1/19/17.
- */
-
-public class TweetTooLongException extends Exception {
-
-}
